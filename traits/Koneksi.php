6a50f4ab09a6517191306fc0b2dd7ac5cabbeef1
xyz970 <muhammadxxz7@gmail.com>
2022-12-14 08:45:12 +0700

    Fix Merged file

--- traits/Koneksi.php
+++ traits/Koneksi.php
@@ -4,11 +4,11 @@ trait Koneksi
 {
     public function mysql()
     {
-        $server = "localhost";
-        $username = "root";
-        $password = "";
-        $db = "nb-carwash";
-        $koneksi = mysqli_connect($server, $username, $password, $db);
+        // $server = "localhost";
+        // $username = "root";
+        // $password = "";
+        // $db = "nb-carwash";
+        $koneksi = mysqli_connect($_ENV['HOST'], $_ENV['MYSQL_USERNAME'], $_ENV['MYSQL_PASSWORD'], $_ENV['MYSQL_DATABASE']);
 
         if (mysqli_connect_error()) {
             echo "Koneksi gagal " . mysqli_connect_error();
