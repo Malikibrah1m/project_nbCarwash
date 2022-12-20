d3f084a80ac865533fb352efe0953280993a5081
xyz970 <muhammadxxz7@gmail.com>
2022-12-21 00:21:24 +0700

    ubah transaksi jadi ajax request, ubah chart di admin dashboard(kurang detail kecil).. akan ditambahkan secepatnya

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,4 +1,7 @@
 <?php
+
+use Carbon\Carbon;
+
 class AdminController extends BaseController
 {
     public function __construct()
@@ -13,8 +16,34 @@ class AdminController extends BaseController
         }
     }
     public function getIndex()
+    { 
+        $year = Carbon::now()->format('Y');
+        $month = Carbon::now()->format('m');
+        $profit = new Profit();
+        $profitPerBulan = $profit->rawQuery("select sum(for_owner) as totalPerBulan from profits where month(date) = $month && year(date) = $year")->get();
+        while ($row = $profitPerBulan->fetch_assoc()) {
+            $data['profitPerbulan'] = $row['totalPerBulan'];
+        }
+        // $data 
+        // print_r($data);
+
+        return $this->view('admin.index',$data);
+    }
+
+    public function getData_tahunan()
     {
-        // print_r($_SESSION['user']['email']);
-        return $this->view('admin.index');
+        $year = Carbon::now()->format('Y');
+        $date = Carbon::now()->format('Y-m-d');
+        $trans = new Transaksi();
+        $listTrans = $trans->rawQuery("select count(*) as y,DATE_FORMAT(date,'%b') as x from transactions where YEAR(date) = $year group by month(date)")->get();
+        $data = [];
+        if ($listTrans->num_rows > 0) {
+            while ($row = $listTrans->fetch_assoc()) {
+                $data[] = $row;
+            }
+        } else {
+            $data = [];
+        }
+        echo json_encode($data);
     }
 }
