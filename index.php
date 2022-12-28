550835ac543a862e4a9f4ea91797518b9c95a148
xyz970 <muhammadxxz7@gmail.com>
2022-12-28 11:31:00 +0700

    change some files

--- index.php
+++ index.php
@@ -22,6 +22,10 @@ define('URL_PATH', $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
 error_reporting(E_ERROR);
 ini_set('display_errors', 'On');
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: *");
+
 
 
 /**
